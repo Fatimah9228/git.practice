90798919e479ec1bc80b7f03d6f67e51f800204e
Fatimah9228 <fatimahiqbal.patel@ada.ac.uk>
2022-11-04 12:40:53 +0000

    if loop

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+#random number gessing user 
+import random
+
+def guess_numb():
+  num = (random.randint(0,10))
+  guess = int(input("enter number between 0 and 10 "))
+  while num != guess:
+    if(guess < num):
+      print("guess too low")
+      guess = int(input("enter number between 0 and 10 "))
+    elif(guess>num):
+      print("guess to high")
+      guess = int(input("enter number between 0 and 10 "))
+    else:
+      print("guess correct")
+      
+    
+guess_numb()
